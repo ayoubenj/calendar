5e97380ba4102dfc154d12d91d62978d2051fb90
ayoubenj <ayoubenj@live.fr>
2018-12-15 19:56:19 +0100

    je

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express'); //express
 var app = express();
-var port =  8080;
-var address = '192.168.1.46';
+var port =   process.env.PORT || 3000;
+var address =  '192.168.1.46' || 'localhost';
+
+app.set('port',port );
+app.set('host', address);
 
 app.get('/', function (req, res) {
       console.log("racine");
   
 })
-.listen(port, address, () => {
-  console.log("Server running on port 5000 s ");
+.listen(app.get('port'), address, () => {
+  console.log("Server running on port 3000 s " +app.get('host') + ':' + app.get('port') );
 });
