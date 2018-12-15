1735f7c86a8c8141f65d135083fe66bd2fd6ec33
ayoubenj <ayoubenj@live.fr>
2018-12-15 21:00:05 +0100

    test3

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var express = require('express'); //express
 var app = express();
-var port =  5000;
-var address =  '127.0.0.1';
 
-app.set('port',port );
-app.set('host', address);
+const port = process.env.PORT || 8000;
 
 app.get('/', function (req, res) {
       console.log("racine");
   
 })
-.listen(app.get('port'), address, () => {
-  console.log("Server running on port 5000 s " +app.get('host') + ':' + app.get('port') );
+.listen(port,  '0.0.0.0', () => {
+    console.log("App is running on port " + port);
 });
